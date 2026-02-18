9a416d328f8d1b64b81de469b71c9e10bd001c49
banker889 <banker889@users.noreply.github.com>
2026-02-18 23:22:23 +0100

    Expand period bins to five ranges and regenerate bin outputs

--- src/grid_frequency_analysis/period_bins.py
+++ src/grid_frequency_analysis/period_bins.py
@@ -13,8 +13,11 @@ class PeriodBin:
 
 
 DEFAULT_PERIOD_BINS: tuple[PeriodBin, ...] = (
-    PeriodBin("primary_local_control", 30.0, 120.0),
+    PeriodBin("fast_sub_primary_5s_to_30s", 5.0, 30.0),
+    PeriodBin("primary_local_control_30s_to_2m", 30.0, 120.0),
+    PeriodBin("midrange_2m_to_15m", 120.0, 900.0),
     PeriodBin("balancing_15m_to_2h", 900.0, 7200.0),
+    PeriodBin("slow_over_2h_to_12h", 7200.0, 43200.0),
 )
 
 
